eab9463a8dffe51e6596cacfdacbe4ebc5bede79
Kyle <Contact@Kylediggs.com>
2019-04-18 11:35:50 -0700

    initial
    Signed-off-by: Kyle <Contact@Kylediggs.com>

---
+++ homework.pl
@@ -0,0 +1,5 @@
+1) The first routine is parsing HTML, that we would like done in Python utilizing BeautifulSoup.  I've attached the info you'll need.  You've been provided an input html file and you need to parse it to create the output json file, as shown.
+
+2) Also in Python, write a compression function that takes an input string s with an example value of "aaabkKdee", that returns this: "3ab2kd2e".
+
+Please feel free to call me before you begin to review expectations, specifications or any other questions you may have.
